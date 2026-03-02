bc4c1aa9c8534dadd1961845b29a7fb38020dce1
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-02 16:31:20 +0000

    Created periodic task in celery

--- web-api/app/celery.py
+++ web-api/app/celery.py
@@ -1,14 +1,24 @@
+from datetime import timedelta
+
 from celery import Celery
+from celery.schedules import crontab
 
 from app.config import Config
 
 celery_worker = Celery(
     "app",
     broker=Config.CELERY_BROKER_URL,
-    result_backend=Config.CELERY_RESULT_BACKEND,
-
+    backend=Config.CELERY_RESULT_BACKEND
 )
+celery_worker.conf.timezone = 'UTC'
+
+celery_worker.conf.beat_schedule = {
+    # Aggregate user billing usage daily at 11pm
+    'aggregate-billing-sunday-00:00': {
+        'task': 'app.tasks.billing.aggregate_current_billing',
+        'schedule': crontab(hour="23"),
+    },
+}
 
 import app.tasks.email
-
-# celery_worker.autodiscover_tasks(["app.tasks"])
+import app.tasks.billing
