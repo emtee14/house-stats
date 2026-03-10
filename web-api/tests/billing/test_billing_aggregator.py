d96fcab41e5b29fac9f144623180c4bbacb00e08
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-10 18:26:12 +0000

    Adding billing aggregation test suite, with stripe integration

--- web-api/tests/billing/test_billing_aggregator.py
+++ web-api/tests/billing/test_billing_aggregator.py
@@ -1,24 +1,15 @@
+import random
 from datetime import datetime, timedelta, UTC
 
 import pytest
 
 from app.billing.billing_aggregator import BillingAggregator
+from app.billing.stripe_adapter import StripePaymentAdapter
 from app.models.billing import Usage
-from tests.common import db_session, create_user, engine, config
+from tests.common import db_session, engine, config
+from tests.auth.common import create_user
+from tests.billing.common import create_usage
 
-@pytest.fixture
-def create_usage(db_session):
-    def _create_usage(user, api_route, tokens, timestamp):
-        new_record = Usage(
-            user_id = user.id,
-            api_route = api_route,
-            tokens = tokens,
-            timestamp = timestamp,
-        )
-        db_session.add(new_record)
-        db_session.commit()
-        return new_record
-    return _create_usage
 
 def test_get_users(db_session, create_user, create_usage):
     user = create_user(email="test@example.com", first_name="John", last_name="Doe", password="Password123")
@@ -78,6 +69,7 @@ def test_aggregate_with_items(db_session, create_user, create_usage):
     create_usage(user=user, api_route="/users/", tokens=bills[3], timestamp=datetime.now(UTC)-timedelta(minutes=2))
 
     ledger = biller.aggregate_user_billing(user)
+    biller.update_usage_events(ledger)
 
     assert ledger.total_tokens == sum(bills)
 
@@ -97,6 +89,7 @@ def test_aggregate_items_in_window(db_session, create_user, create_usage):
     create_usage(user=user, api_route="/users/", tokens=bills[3], timestamp=datetime.now(UTC)+timedelta(minutes=2))
 
     ledger = biller.aggregate_user_billing(user, upto=current_time)
+    biller.update_usage_events(ledger)
 
     assert ledger.total_tokens == sum(bills)-bills[-1]
 
@@ -122,7 +115,10 @@ def test_aggregate_two_ledgers(db_session, create_user, create_usage):
     create_usage(user=user, api_route="/users/", tokens=bills_2[3], timestamp=current_time + timedelta(minutes=2))
 
     ledger_1 = biller.aggregate_user_billing(user, upto=current_time)
+    biller.update_usage_events(ledger_1)
+
     ledger_2 = biller.aggregate_user_billing(user, upto=current_time+timedelta(minutes=3))
+    biller.update_usage_events(ledger_2)
 
     assert ledger_1.total_tokens == sum(bills_1)
     assert ledger_2.total_tokens == sum(bills_2)
@@ -144,9 +140,98 @@ def test_aggregate_two_ledgers_one_empty(db_session, create_user, create_usage):
 
 
     ledger_1 = biller.aggregate_user_billing(user, upto=current_time)
+    biller.update_usage_events(ledger_1)
     with pytest.raises(ValueError) as e:
         ledger_2 = biller.aggregate_user_billing(user, upto=current_time+timedelta(minutes=3))
 
 
     assert ledger_1.total_tokens == sum(bills_1)
     assert str(e.value) == "User has no usage recorded"
+
+
+def test_aggregation_en_masse_single_user(create_user, create_usage, db_session, config):
+    user = create_user(email="test@example.com", first_name="John", last_name="Doe", password="Password123")
+
+    stripe_adap = StripePaymentAdapter(config.STRIPE_API_TOKEN)
+    stripe_adap.setup_user(user, db_session)
+
+    bills = [50, 100, 150, 200]
+
+    usages = [
+        create_usage(user=user, api_route="/users/", tokens=bills[0],
+                     timestamp=datetime.now(UTC) - timedelta(minutes=2)),
+        create_usage(user=user, api_route="/users/", tokens=bills[1],
+                     timestamp=datetime.now(UTC) - timedelta(minutes=2)),
+        create_usage(user=user, api_route="/users/", tokens=bills[2],
+                     timestamp=datetime.now(UTC) - timedelta(minutes=2)),
+        create_usage(user=user, api_route="/users/", tokens=bills[3],
+                     timestamp=datetime.now(UTC) - timedelta(minutes=2))
+    ]
+
+
+    biller = BillingAggregator(db_session)
+
+    agg_event = biller.aggregate_en_masse(billing_hour=datetime.now(UTC).hour+1)
+
+    ledgers = agg_event.billing_ledgers
+    cost_items = ledgers[0].cost_items
+
+    for usage in usages:
+        assert usage in cost_items
+
+
+def test_aggregation_en_masse_multi_user(create_user, create_usage, db_session, config, seed="kjfnkjanfkjwnfkwaj"):
+    user_1 = create_user(email="test@example.com", first_name="John", last_name="Doe", password="Password123")
+    user_2 = create_user(email="test-1@example.com", first_name="John", last_name="Doe", password="Password123")
+    user_3 = create_user(email="test-2@example.com", first_name="John", last_name="Doe", password="Password123")
+
+    stripe_adap = StripePaymentAdapter(config.STRIPE_API_TOKEN)
+    stripe_adap.setup_user(user_1, db_session)
+    stripe_adap.setup_user(user_2, db_session)
+    stripe_adap.setup_user(user_3, db_session)
+
+    random.seed(seed)
+
+    bills_1 = [random.randint(0,500) for i in range(50)]
+    bills_2 = [random.randint(0, 500) for i in range(50)]
+    bills_3 = [random.randint(0, 500) for i in range(50)]
+
+    usages_1 = []
+    usages_2 = []
+    usages_3 = []
+
+    for bill in bills_1:
+        usages_1.append(
+            create_usage(user=user_1, api_route="/users/", tokens=bill,
+                         timestamp=datetime.now(UTC) - timedelta(minutes=10))
+        )
+
+    for bill in bills_2:
+        usages_2.append(
+            create_usage(user=user_2, api_route="/users/", tokens=bill,
+                         timestamp=datetime.now(UTC) - timedelta(minutes=10))
+        )
+
+    for bill in bills_3:
+        usages_3.append(
+            create_usage(user=user_3, api_route="/users/", tokens=bill,
+                         timestamp=datetime.now(UTC) - timedelta(minutes=10))
+        )
+
+    biller = BillingAggregator(db_session)
+
+    agg_event = biller.aggregate_en_masse(billing_hour=datetime.now(UTC).hour+1)
+
+    ledgers = agg_event.billing_ledgers
+    for ledger in ledgers:
+        match ledger.user_id:
+            case user_1.id:
+                for usage in usages_1:
+                    assert usage in ledger.cost_items
+            case user_2.id:
+                for usage in usages_2:
+                    assert usage in ledger.cost_items
+            case user_3.id:
+                for usage in usages_2:
+                    assert usage in ledger.cost_items
+
--- web-api/tests/billing/test_stripe_adapter.py
+++ web-api/tests/billing/test_stripe_adapter.py
@@ -5,22 +5,12 @@ import stripe
 from dotenv import load_dotenv
 
 from app.billing.stripe_adapter import StripePaymentAdapter
+from tests.common import config
 
 
-@pytest.fixture
-def stripe_api_key():
-    print(os.getcwd())
-    load_dotenv("../.env.dev")
-    api_key = os.environ.get('STRIPE_API_TOKEN')
-    return api_key
-
-
-def test_initial_login(stripe_api_key):
-    if stripe_api_key is None:
-        pytest.fail("Stripe API key not set")
-
+def test_initial_login(config):
     try:
-        stripe_adap = StripePaymentAdapter(stripe_api_key)
+        stripe_adap = StripePaymentAdapter(config.STRIPE_API_TOKEN)
     except stripe.error.AuthenticationError:
         pytest.fail("Authentication Error")
 
@@ -32,6 +22,3 @@ def test_failed_login():
         assert stripe_adap.connected == False
     assert exc_info.value.args[0].startswith("Invalid API Key provided:") is True
 
-
-if __name__ == "__main__":
-    stripe_api_key = os.environ.get('STRIPE_API_KEY')
