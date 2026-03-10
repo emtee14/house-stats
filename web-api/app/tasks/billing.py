4473cbf1e9a8ba5ab98fc31e94ef03ba952a793f
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-10 18:25:32 +0000

    Adding billing agg task

--- web-api/app/tasks/billing.py
+++ web-api/app/tasks/billing.py
@@ -1,10 +1,17 @@
-from datetime import datetime
+import time
+import uuid
+from datetime import datetime, timedelta
+from typing import List
 from uuid import UUID
 
 from sqlmodel import Session
 
+from app.billing.billing_aggregator import BillingAggregator
+from app.billing.stripe_adapter import StripePaymentAdapter
 from app.celery import celery_worker
-from app.models.billing import Usage
+from app.config import Config
+from app.models.auth import User
+from app.models.billing import Usage, AggregationEvent, BillingLedger
 from app.tasks.base_task import DatabaseTask
 
 
@@ -18,11 +25,16 @@ def log_token_usage(user_id: UUID, endpoint: str,
         tokens=token_amount,
         timestamp=bill_time,
     )
+
     session.add(new_charge)
     session.commit()
     print(f"Logged user {user_id} endpoint usage, charged {token_amount} tokens")
 
 
 @celery_worker.task(base=DatabaseTask)
-def aggregate_current_billing(session: Session = None):
-    print("Aggregating current billing")
+def aggregate_current_billing(session: Session = None, billing_hour: int = 23) -> uuid.UUID:
+    aggregator = BillingAggregator(session)
+
+    event = aggregator.aggregate_en_masse(billing_hour)
+
+    return event.id
