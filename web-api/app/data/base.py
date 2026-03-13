c156f473316a50a7277ec5e3a256346d2d48f80a
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-13 00:36:07 +0000

    Swapped to use db session instead of engine

--- web-api/app/data/base.py
+++ web-api/app/data/base.py
@@ -2,16 +2,16 @@ from typing import Dict
 
 import polars as pl
 from sqlalchemy import text
+from sqlmodel import Session
 
 
 class BaseDataset:
-    def __init__(self, db):
-        self._engine = db
+    def __init__(self, session: Session):
+        self._session = session
 
     def run_query(self, sql: str, parameters: Dict):
-        with self._engine.connect() as conn:
-            result = conn.execute(text(sql), parameters)
-            columns = result.keys()
-            rows = result.fetchall()
+        result = self._session.execute(text(sql), parameters)
+        columns = list(result.keys())
+        rows = result.fetchall()
 
         return pl.DataFrame(rows, schema=columns)
