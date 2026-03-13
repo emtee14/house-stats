726a037a6e687ede159e07d3cce99376bf999240
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-13 22:45:26 +0000

    Fixed tests

--- web-api/tests/data/test_sales_dataset.py
+++ web-api/tests/data/test_sales_dataset.py
@@ -1,10 +1,12 @@
 from datetime import datetime, UTC
 
-from tests.common import config, engine
+from sqlmodel import Session
+
+from tests.common import config, engine, db_session
 from app.data.sales import SalesDataset
 
-def test_sales_dataset(engine):
-    dataset = SalesDataset(engine)
+def test_sales_dataset(db_session: Session):
+    dataset = SalesDataset(db_session)
     df = dataset.query("area", "LS",
                        datetime(1995, 1, 1), datetime.now(UTC))
     print(df)
