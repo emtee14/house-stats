ab52b93661e6e56cca6741922fa0f600d5072a4c
emtee14 <sc23mt2@leeds.ac.uk>
2026-03-14 23:30:38 +0000

    Fixed billing sched task

--- web-api/app/celery.py
+++ web-api/app/celery.py
@@ -15,7 +15,7 @@ celery_worker.conf.timezone = "UTC"
 celery_worker.conf.beat_schedule = {
     # Aggregate user billing usage daily at 11pm
     "aggregate-billing-everyday-23:00": {
-        "task": "app.tasks.billing.sched_aggregate_current_billing",
+        "task": "app.tasks.billing.aggregate_current_billing",
         "schedule": crontab(hour="23"),
     },
 }
